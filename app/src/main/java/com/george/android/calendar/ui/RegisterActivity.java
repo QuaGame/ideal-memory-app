7da13077102bc55dea2041fc7a2acdf48db0d5d2
Timofey <temosmitenev@gmail.com>
2024-02-05 21:37:44 +0300

    add register_login.xml and RegisterActivity.java

---
+++ app/src/main/java/com/george/android/calendar/ui/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.george.android.calendar.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.george.android.calendar.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_login);
+    }
+}
